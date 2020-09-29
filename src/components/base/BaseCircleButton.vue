cb825f2f6328d3a0a809cd14f5f4f2741ec6a1da
Alessio Farinelli <alessio.farinelli.developer@gmail.com>
2020-09-29 13:17:08 +0200

    BaseCircloButton added and some Icons

---
+++ src/components/base/BaseCircleButton.vue
@@ -0,0 +1,42 @@
+<template>
+   <button :style="buttonStyle">
+      <slot></slot>
+   </button>
+</template>
+
+<script>
+export default {
+   name: 'BaseCircleButton',
+   props: {
+      side: {
+         type: Number,
+         default: 100
+      },
+      color: {
+         type: String,
+         default: '#FE655850'
+      }
+   },
+   data() {
+      return {
+         buttonStyle: {
+            width: this.side + 'vw',
+            height: this.side + 'vw',
+            'border-radius': this.side / 2 + 'vw',
+            'background-color': this.color
+         }
+      }
+   }
+}
+</script>
+
+<style lang="sass" scoped>
+button
+   border: none
+   display: flex
+   justify-content: center
+   align-items: center
+   transition: all 50ms linear
+   &:hover
+      filter: contrast(200%)
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,38 @@
 <template>
    <BaseInput width="25" height="7" placeholder="password" :active="false" />
+   <BaseCircleButton :side="3" @click="socialRegister('google')">
+      <BaseIcon width="20" height="21" viewBox="0 0 20 21">
+         <GoogleIcon />
+      </BaseIcon>
+   </BaseCircleButton>
+   <BaseCircleButton :side="3" @click="socialRegister('twitter')">
+      <BaseIcon width="24" height="21" viewBox="0 0 24 21">
+         <TwitterIcon />
+      </BaseIcon>
+   </BaseCircleButton>
+   <BaseCircleButton :side="3" @click="socialRegister('twitter')">
+      <BaseIcon width="25" height="25" viewBox="0 0 25 25">
+         <FacebookIcon />
+      </BaseIcon>
+   </BaseCircleButton>
 </template>
 
 <script>
 import BaseInput from '@/components/base/BaseInput.vue'
+import BaseCircleButton from '@/components/base/BaseCircleButton.vue'
+import BaseIcon from '@/components/base/BaseIcon.vue'
+import GoogleIcon from '@/assets/icons/GoogleIcon.vue'
+import TwitterIcon from '@/assets/icons/TwitterIcon.vue'
+import FacebookIcon from '@/assets/icons/FacebookIcon.vue'
 
 export default {
    name: 'Home',
-   components: { BaseInput }
+   components: { BaseInput, BaseCircleButton, BaseIcon, GoogleIcon, TwitterIcon, FacebookIcon },
+   methods: {
+      socialRegister(type) {
+         console.log(type)
+      }
+   }
 }
 </script>
 
